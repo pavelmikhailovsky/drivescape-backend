4e14427929d1488aeed84a3a1902077c680faf72
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-11-14 23:35:51 +0300

    override method for registry json

--- src/main/java/by/drivescape/configs/AppConfig.java
+++ src/main/java/by/drivescape/configs/AppConfig.java
@@ -5,12 +5,22 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("by.drivescape")
-public class AppConfig {
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer{
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.enableContentNegotiation(new MappingJackson2JsonView());
+    }
 
     @Bean
     public DataSource dataSource() {
