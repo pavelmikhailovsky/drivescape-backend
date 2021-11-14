9d34b0dfe09455b5f4202f54c05a7dadfb996987
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-11-14 23:36:23 +0300

    updated getById method

--- src/main/java/by/drivescape/dao/DataAccessObject.java
+++ src/main/java/by/drivescape/dao/DataAccessObject.java
@@ -1,12 +1,13 @@
 package by.drivescape.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface DataAccessObject<T> {
 
     List<T> getAll();
 
-    T getById(Long id);
+    T getById(Long id) throws SQLException;
 
     void create(T t);
 }
