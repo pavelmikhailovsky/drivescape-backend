d7f55d8eaaac73b6e045810bdd753ca5607e1df0
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-11-14 23:39:31 +0300

    corrected methods for using jdbctemplate

--- src/main/java/by/drivescape/dao/PersonDAO.java
+++ src/main/java/by/drivescape/dao/PersonDAO.java
@@ -2,31 +2,41 @@ package by.drivescape.dao;
 
 import by.drivescape.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+import java.sql.SQLException;
 import java.util.List;
 
-@Component
-public class PersonDAO {
+@Repository
+public class PersonDAO implements DataAccessObject<Person> {
 
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
     public PersonDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        System.out.println(PersonDAO.class);
     }
 
     public List<Person> getAll() {
-        return jdbcTemplate.queryForList("SELECT * FROM person ORDER BY person_id DESC", Person.class);
+        return jdbcTemplate.query(
+                "SELECT * FROM person ORDER BY person_id DESC",
+                new BeanPropertyRowMapper<>(Person.class)
+        );
     }
 
-    public Person getById(Long id) {
-        return jdbcTemplate.queryForObject("SELECT * FROM person WHERE person_id=?", Person.class, id);
+    public Person getById(Long id) throws SQLException {
+        return jdbcTemplate.query(
+                "SELECT * FROM person WHERE person_id=?",
+                new BeanPropertyRowMapper<>(Person.class),
+                id
+        ).stream().findAny().orElseThrow(() -> new SQLException("Person is not find by id"));
     }
 
     public void create(Person person) {
-        jdbcTemplate.update(
+         jdbcTemplate.update(
                 "INSERT INTO person(first_name, last_name, password, email) VALUES (?, ?, ?, ?)",
                 person.getFirstName(), person.getLastName(), person.getPassword(), person.getEmail()
         );
