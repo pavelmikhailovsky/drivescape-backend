823c8d31080bb25eeedc1f01677c627f8cf09d51
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-11-14 23:40:53 +0300

    added examination length password and created two methods to receive person

--- src/main/java/by/drivescape/services/PersonService.java
+++ src/main/java/by/drivescape/services/PersonService.java
@@ -5,6 +5,9 @@ import by.drivescape.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
+import java.util.List;
+
 @Service
 public class PersonService {
 
@@ -13,14 +16,25 @@ public class PersonService {
 
     @Autowired
     public PersonService(PersonDAO personDAO, HashingService hashingService) {
+        System.out.println(PersonService.class);
         this.personDAO = personDAO;
         this.hashingService = hashingService;
     }
 
-    public void createUser(Person person) {
+    public void createPerson(Person person) throws MinLengthException {
         String password = person.getPassword();
+        if(password.length() < 8) {
+            throw new MinLengthException("Length password should be min 8 character");
+        }
         person.setPassword(hashingService.getHashString(password));
         personDAO.create(person);
     }
 
+    public Person getByIdPerson(Long id) throws SQLException {
+        return personDAO.getById(id);
+    }
+
+    public List<Person> getAllPerson() {
+        return personDAO.getAll();
+    }
 }
